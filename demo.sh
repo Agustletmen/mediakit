c4c7c463123e61fc8e7294d5b56ee9c78935fe4f
Your Name <you@example.com>
2025-12-18 03:40:04 +0000

    feat：添加海康库文件

---
+++ demo.sh
@@ -0,0 +1,73 @@
+# opencv
+apt install libopencv-dev
+apt install libopencv-core-dev
+apt install libopencv-highgui-dev
+apt install libopencv-calib3d-dev
+apt install libopencv-features2d-dev
+apt install libopencv-imgproc-dev
+apt install libopencv-video-dev
+
+# ffmpeg
+apt install libavdevice-dev
+apt install libavutil-dev
+apt install libavcodec-dev
+apt install libavformat-dev
+apt install libavfilter-dev
+apt install libswscale-dev
+apt install libswresample-dev
+
+# 编码库
+apt install libsdl2-dev
+apt install libx264-dev
+apt install libx265-dev
+apt install libvpx-dev
+apt install libfdk-aac-dev
+apt install libmp3lame-dev
+apt install libopus-dev
+apt install libvorbis-dev
+
+apt install libsrt-dev
+
+# 字幕
+apt install libass-dev
+apt install libfreetype6-dev
+
+# X11 图形系统依赖
+apt install libxcb1-dev
+apt install libxcb-shm0-dev
+apt install libxcb-xfixes0-dev
+
+apt install libssl-dev
+apt install libssh-dev
+
+apt install libgtk-3-dev
+apt install libglfw3-dev
+apt install libopenblas-dev
+
+
+apt install libstdc++-dev
+apt install libc-dev
+apt install libpthread-dev
+apt install libm-dev
+
+apt install zlib1g-dev
+apt install libboost-all-dev
+apt install libbz2-dev
+apt install libz-dev
+
+
+apt install pkg-config
+apt install build-essential
+# gcc：C 语言编译器
+# g++：C++ 语言编译器
+# make：自动化构建工具（解析 Makefile，执行编译步骤）
+# libc6-dev：GNU C 标准库的开发文件（头文件、静态库等）
+
+apt install binutils # GNU 二进制工具集
+# as：汇编器（将汇编语言代码转换为机器码）
+# ld：链接器（将多个目标文件链接成可执行文件或库文件）
+# objdump：查看二进制文件的反汇编代码、符号表等信息
+# nm：列出二进制文件中的符号（函数、变量名）
+# strip：移除二进制文件中的调试信息，减小文件体积
+# ar：创建和管理静态库（.a 文件）
+
